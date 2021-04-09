1a4400f37303993c9bbb40fabc76073ff59c9063
Saia Fonua <saia.fonua@C02F20S7MD6M.local>
2021-04-09 13:04:20 -0600

    WIP text displaying correctly on language change

---
+++ data/colors.js
@@ -0,0 +1,32 @@
+export const colors = [
+    {
+        id: 0,
+        englishText: 'green',
+        spanishText: 'verde',
+        tonganText: 'lanumata'
+    },
+    {
+        id: 1,
+        englishText: 'red',
+        spanishText: 'rojo',
+        tonganText: 'kulokula'
+    },
+    {
+        id: 2,
+        englishText: 'white',
+        spanishText: 'blanco',
+        tonganText: 'hinehina'
+    },
+    {
+        id: 3,
+        englishText: 'yellow',
+        spanishText: 'amarillo',
+        tonganText: 'engeenga'
+    },
+    {
+        id: 4,
+        englishText: 'blue',
+        spanishText: 'azul',
+        tonganText: 'lanumoana'
+    },
+];
